8b94d43d32fc1c63760cc2f10cb91adc1e7a2217
Rohan <rohan.mudliar@gmail.com>
2021-10-06 03:28:30 +0530

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,104 @@
+import styled from "styled-components";
+
+function Header() {
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg" />
+          <span>Home</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" />
+          <span>Search</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" />
+          <span>Watch</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" />
+          <span>original</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" />
+          <span>Movies</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" />
+          <span>Series</span>
+        </a>
+      </NavMenu>
+
+      <UserImg src="https://avatars.githubusercontent.com/u/44732632?v=4" />
+    </Nav>
+  );
+}
+
+export default Header;
+
+const Nav = styled.nav`
+  height: 70px;
+  background-color: #090b1b;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 25px;
+  align-items: center;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+
+    img {
+      height: 20px;
+    }
+
+    span {
+      font-size: 13px;
+      letter-spacing: 1.2px;
+      text-transform: uppercase;
+      position: relative;
+
+      &:after {
+        content: "";
+        height: 2px;
+        background-color: white;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity: 0;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms ease-out;
+      }
+    }
+
+    &:hover {
+      span:after {
+        transform: scaleX(1);
+        opacity: 1;
+      }
+    }
+  }
+`;
+
+const UserImg = styled.img`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  cursor: pointer;
+`;
