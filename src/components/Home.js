86ff28015b1459d226167d3a2b9aa959d9763ab8
Rohan <rohan.mudliar@gmail.com>
2021-10-06 22:19:05 +0530

    added moviespage

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,14 @@ import ImgSlider from "./ImgSlider";
 
 import styled from "styled-components";
 import Viewers from "./Viewers";
+import Movies from "./Movies";
 
 function Home() {
   return (
     <Container>
       <ImgSlider />
       <Viewers />
+      <Movies />
     </Container>
   );
 }
